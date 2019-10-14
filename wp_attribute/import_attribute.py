064a20ceefa650c18254670bc7962824c8983821
nicothebrush <nicola.riolini@gmail.com>
2019-10-14 14:10:01 +0200

    generic bugfix (fast message)

--- wp_attribute/import_attribute.py
+++ wp_attribute/import_attribute.py
@@ -44,6 +44,19 @@ from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
 
 _logger = logging.getLogger(__name__)
 
+class ProductProduct(orm.Model):
+    """ Model name: ProductProduct
+    """
+
+    _inherit = 'product.product'
+    
+    _columns = {
+        'wp_parent_template': fields.boolean(
+            'Template for variants', 
+            help='Product used as tempalte for variants (first six char of '
+                'code)'),
+        }
+    
 '''class ProductPublicCategory(orm.Model):
     """ Model name: ProductProduct
     """
@@ -75,7 +88,7 @@ class ProductPublicCategory(orm.Model):
         #                         WORDPRESS Publish:
         # ---------------------------------------------------------------------
         server_pool = self.pool.get('connector.server')
-        #category_pool = self.pool.get('product.public.category')
+        web_product_pool = self.pool.get('product.product.web.server')
         
         # ---------------------------------------------------------------------
         #                        CREATE CATEGORY OPERATION:
@@ -89,10 +102,10 @@ class ProductPublicCategory(orm.Model):
             cr, uid, connector_id, context=context)
 
         # ---------------------------------------------------------------------        
-        # Read all category:
+        # Read all attributes:
         # ---------------------------------------------------------------------        
         current_wp_attribute = wcapi.get(
-            'products/attributes', params=parameter).json()
+            'products/attributes').json()
 
         try:
             test_error = res['data']['status'] == 400
@@ -102,212 +115,152 @@ class ProductPublicCategory(orm.Model):
                 )
         except:
             pass # no error               
-        import pdb; pdb.set_trace()
-        # Search "Tessuto"
-        
-        
-        import pdb; pdb.set_trace()
+
         # ---------------------------------------------------------------------        
-        # Loading used dict DB
+        # Search Tessuto attribute:
         # ---------------------------------------------------------------------        
-        """
-        wp_db = {}
-        wp_name = {} # TODO manage!
-        for record in current_wp_category:
-            wp_db[record['id']] = record['name']
-            wp_name[(record['parent'] or False, record['name'])] = record['id']
+        attribute_id = {
+            'Tessuto': False,
+            'Brand': False,
+            }
+        _logger.warning('Searching attribute %s...' % (attribute_id.keys() ))
+        
+        for record in current_wp_attribute:
+            if record['name'] in attribute_id:
+                attribute_id[record['name']] = record['id']
+        
+        if not all(attribute_id.values()):
+            raise osv.except_osv(
+                _('Attribute error'), 
+                _('Cannot find attribute %s!') % (attribute_id, ),
+                )        
+        
+        # ---------------------------------------------------------------------        
+        # Read current attributes terms:
+        # ---------------------------------------------------------------------        
+        current_wp_terms = []
+        theres_data = True
+        parameter = {
+            'per_page': 10,
+            'page': 0,
+            }
+        
+        _logger.warning('Search all terms for attribute %s...' % (
+            attribute_id.keys(), ))
+        while theres_data:
+            parameter['page'] += 1
+            res = wcapi.get(
+                'products/attributes/%s/terms' % attribute_id['Tessuto'], 
+                params=parameter).json()
 
-        # ---------------------------------------------------------------------
-        #                                Mode IN:
-        # ---------------------------------------------------------------------
-        # TODO Language management!!!
-        if server_proxy.wp_category == 'in':
-            _logger.warning(
-                'Mode IN Wordpress category import [# %s]' % len(
-                    current_wp_category))
-
-            parent_wp_db = {}
-            # Sorted so parent first:
-            for record in sorted(current_wp_category, 
-                    key=lambda x: x['parent']):
-                wp_id = record['id']
-                parent_id = record['parent'] or False
-                name = record['name']
+            try:
+                test_error = res['data']['status'] == 400
+                raise osv.except_osv(
+                    _('Category error:'), 
+                    _('Error getting category list: %s' % (res, ) ),
+                    )
+            except:
+                pass # no error               
                 
-                category_ids = category_pool.search(cr, uid, [
-                    ('connector_id', '=', connector_id),
-                    ('wp_id', '=', wp_id),
-                    ], context=context)
-                if category_ids:
-                    odoo_id = category_ids[0]
-                    category_pool.write(cr, uid, category_ids, {
-                        'parent_id': parent_wp_db.get(
-                            parent_id, False),
-                        'name': name,
-                        'sequence': record['menu_order'], 
-                        }, context=context)                    
-                    _logger.info('Update %s' % name)    
-                else:                   
-                    odoo_id = category_pool.create(cr, uid, {
-                        'enabled': True,
-                        'connector_id': connector_id,
-                        'wp_id': wp_id,
-                        'parent_id': parent_wp_db.get(
-                            parent_id, False),
-                        'name': name,
-                        'sequence': record['menu_order'], 
-                        }, context=context)
-                    _logger.info('Create %s' % name)    
-                   
-                # Save root parent ID: 
-                parent_wp_db[wp_id] = odoo_id
-            return True
-                        
-        # ---------------------------------------------------------------------
-        #                                Mode OUT:
-        # ---------------------------------------------------------------------
-        # A. Read ODOO PARENT category:
-        # ---------------------------------------------------------------------
-        _logger.warning('Mode OUT Wordpress category export')
-        odoo_parent = {}
-        category_ids = category_pool.search(cr, uid, [
-            ('connector_id', '=', connector_id),
-            ('parent_id', '=', False),
-            ], context=context)
+            if res:
+                current_wp_terms.extend(res)
+            else:
+                theres_data = False
 
-        for category in category_pool.browse(
-                cr, uid, category_ids, context=context):    
-            wp_id = category.wp_id            
-            odoo_id = category.id
-            name = category.name
-            
-            record_data = {
-                'name': name,
-                'parent': 0,
-                'menu_order': category.sequence,
-                'display': 'default',
-                }
+        web_attribute = {}
+        for record in current_wp_terms:
+            web_attribute[record['name']] = record['id']
 
-            # Check if present (same name or ID):            
-            key = (False, name)
-            if key in wp_name:
-                wp_id = wp_name[key]
-                # Update this wp_id (same name)
-                category_pool.write(cr, uid, [category.id], {
-                    'wp_id': wp_id,
-                    }, context=context)
-                
-            if wp_id in wp_db: # Update (ID or Name present)
-                record_data['id'] = wp_id
-                data['update'].append(record_data)
-                try:
-                    del(wp_db[wp_id])
-                except:
-                    pass # yet deleted (from Front end?)
-    
-            else: # Create:
-                data['create'].append(record_data)
-                odoo_parent[name] = odoo_id
-
-        res = wcapi.post('products/categories/batch', data).json()
-        for record in res.get('create', ()):
-            wp_id = record['id']
-            if not wp_id:
-                # TODO manage error:
-                _logger.error('Not Updated wp_id for %s' % wp_id)
+        # ---------------------------------------------------------------------
+        # Generate attribute terms from product:
+        # ---------------------------------------------------------------------
+        product_ids = web_product_pool.search(cr, uid, [], context=context)
+        product_db = {}
+        attribute_db = []
+        for record in sorted(web_product_pool.browse(cr, uid, product_ids, 
+                context=context), 
+                key=lambda x: x.product_id.wp_parent_template, reverse=True):
+            # First is the template (if present)
+            product = record.product_id
+            default_code = (product.default_code or '')[:12]
+            if not default_code[:3].isdigit():
                 continue
-
-            name = record['name']
-            odoo_id = odoo_parent.get(name, False)
-            if not odoo_id:
-                _logger.error('Not Updated wp_id for %s' % name)
-                continue
-
-            category_pool.write(cr, uid, odoo_id, {
-                'wp_id': record['id'],
-                }, context=context)
-            odoo_parent[name] = odoo_id
-            _logger.info('Updated wp_id for %s' % name)
-
-        # ---------------------------------------------------------------------
-        # B. Read ODOO category child:
-        # ---------------------------------------------------------------------
-        odoo_child = {}
-        data = {'create': [], 'update': []}
-        category_ids = category_pool.search(cr, uid, [
-            ('connector_id', '=', connector_id),
-            ('parent_id', '!=', False),
-            ], context=context)
-
-        for category in category_pool.browse(
-                cr, uid, category_ids, context=context):    
-            wp_id = category.wp_id            
-            odoo_id = category.id
-            name = category.name
-            parent_wp_id = category.parent_id.wp_id
+            product_parent = default_code[:6].strip()
+            product_attribute = '%s-%s' % (
+                default_code[6:8].strip().upper() or 'NE', # XXX Neutro
+                default_code[8:].strip().upper(),
+                )
+            if product_attribute not in attribute_db:
+                attribute_db.append(product_attribute)
             
-            record_data = {
-                'name': name,
-                'parent': parent_wp_id,
-                'menu_order': category.sequence,
-                'display': 'default',
+            if product_parent not in product_db:
+                product_db[product_parent] = [
+                    product, # Template product
+                    [], # Variant product
+                    ]
+            product_db[product_parent][1].append((product, product_attribute))
+            # Extract frame-color from code
+
+        data = {
+            'create': [],
+            'update': [],
+            'delete': [],
+            }
+        
+        # ---------------------------------------------------------------------
+        # Update / Create:
+        # ---------------------------------------------------------------------
+        for attribute in attribute_db:
+            item = {
+                'name': attribute,
+                # 'color': # XXX RGP color
                 }
-
-            # Check if present :
-            key = (parent_wp_id, name)
-            if key in wp_name:
-                wp_id = wp_name[key]
-                # Update this wp_id (same name)
-                category_pool.write(cr, uid, [category.id], {
-                    'wp_id': wp_id,
-                    }, context=context)
-
-            if wp_id in wp_db: # Update
-                record_data['id'] = wp_id
-                data['update'].append(record_data)
-                try:
-                    del(wp_db[wp_id])
-                except:
-                    pass # yet deleted (from Front end?)
-    
-            else: # Create:
-                data['create'].append(record_data)
-                odoo_child[name] = odoo_id
+            if attribute in web_attribute:
+                data['update'].append(item)
+            else:
+                data['create'].append(item)
 
         # ---------------------------------------------------------------------
-        #                     UPDATE / DELETE CATEGORY OPERATION:
+        # Delete:
         # ---------------------------------------------------------------------
-        data['delete'] = wp_db.keys()
+        # XXX Not for now:
+        #for name in web_attribute:
+        #    if name not in attribute_db:
+        #        data['delete'].append(web_attribute[name])
+
+        # ---------------------------------------------------------------------
+        # Batch operation:
+        # ---------------------------------------------------------------------
+        import pdb; pdb.set_trace()
         try:
-            res = wcapi.post('products/categories/batch', data).json()
+            res = wcapi.post(
+                'products/attributes/%s/terms/batch' % attribute_id, 
+                data=data,
+                ).json()
         except:
             raise osv.except_osv(
                 _('Error'), 
                 _('Wordpress server not answer, timeout!'),
                 )
+        # TODO check result for res
+    
+        # ---------------------------------------------------------------------        
+        # Upload product template:
+        # ---------------------------------------------------------------------
+        parent_unset = []
+        for parent in product_db:
+            product, variants = product_db[parent]
+            if not product.wp_parent_template:
+                parent_unset.append(parent)
+                continue
+        
+            # -----------------------------------------------------------------
+            # Upload product variations:
+            # -----------------------------------------------------------------
+            
+        if parent_unset:
+            _logger.error('Set parent for code start with: %s' % (
+                parent_unsed))
+
                         
-        # Update category with WP ID:
-        for record in res.get('create', ()):
-            wp_id = record['id']
-            if not wp_id:
-                # TODO manage error:
-                _logger.error('Not Updated wp_id for %s' % wp_id)
-                continue
-                
-            name = record['name']
-            odoo_id = odoo_child.get(name, False)
-            if not odoo_id:
-                _logger.error('Not Updated wp_id for %s' % name)
-                continue
-                
-            category_pool.write(cr, uid, odoo_id, {
-                'wp_id': record['id'],
-                }, context=context)
-            _logger.info('Updated wp_id for %s' % name)
-        return True
-        # TODO      
-        # Check updated
-        # Check deleted
-        # Update product first category?
-        """
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
