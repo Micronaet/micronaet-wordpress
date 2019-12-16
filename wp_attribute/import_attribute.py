eec515f6c14259c0945941b1373453ba3c9e3104
nicothebrush <nicola.riolini@gmail.com>
2019-12-16 17:18:25 +0100

    generic bugfix (fast message)

--- wp_attribute/import_attribute.py
+++ wp_attribute/import_attribute.py
@@ -305,8 +305,8 @@ class ProductPublicCategory(orm.Model):
         # =====================================================================
         # Log operation on Excel file:
         # ---------------------------------------------------------------------
-        ws_name = ('Chiamate %s' % datetime.now()).replace('/', ' ').replace(
-            '-', ' ')
+        now = datetime.now()).replace('/', '').replace('-', '')[:30]
+        ws_name = now
         excel_pool = self.pool.get('excel.writer')        
         excel_pool.create_worksheet(ws_name)
         excel_pool.set_format()
