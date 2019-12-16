c9cfcf02e30e65bf6f7a2515e2a7e56a7b3fdc34
nicothebrush <nicola.riolini@gmail.com>
2019-12-16 17:04:42 +0100

    generic bugfix (fast message)

--- wp_attribute/import_attribute.py
+++ wp_attribute/import_attribute.py
@@ -358,7 +358,6 @@ class ProductPublicCategory(orm.Model):
             ('connector_id', '=', ids[0]),
             ('wp_parent_template', '=', True),
             ], context=context)
-        product_ids = product_ids[0:5]  # TODO remove!!!!!!!!!!!!!!!!!!!!!!!!!!
         _logger.warning('Product for this connector: %s...' % len(product_ids))
 
         product_db = {} # Master database for lang - parent - child
