8c8a992cc47fa1dbdb353390297bc8ce37bc1e8a
nicothebrush <nicola.riolini@gmail.com>
2019-12-18 10:06:43 +0100

    generic bugfix (fast message)

--- wp_attribute/import_attribute.py
+++ wp_attribute/import_attribute.py
@@ -345,6 +345,7 @@ class ProductPublicCategory(orm.Model):
         product_ids = web_product_pool.search(cr, uid, [
             ('connector_id', '=', ids[0]),
             ('wp_parent_template', '=', True),
+            ('product_id.default_code', '=', '127   BIJUT'), # REMOVE XXX
             ], context=context)
         #product_ids = product_ids[:3]  # XXX remove!!!
         _logger.warning('Product for this connector: %s...' % len(product_ids))
@@ -353,6 +354,7 @@ class ProductPublicCategory(orm.Model):
         lang_color_db = {} # Master list for color in default lang
         product_default_color = {} # First variant showed
 
+        import pdb; pdb.set_trace()
         parent_total = 0
         for odoo_lang in langs:
             lang = odoo_lang[:2]
