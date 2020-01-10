3163c629400e8bb7878c26e984857a74a6c0d0ea
nicothebrush <nicola.riolini@gmail.com>
2020-01-10 15:15:55 +0100

    generic bugfix (fast message)

--- script/update/external/update_master_product.py
+++ script/update/external/update_master_product.py
@@ -114,7 +114,7 @@ for company in company_list:
     select_pool = pools[company][lang]['web_product']
     web_product_ids = select_pool.search([
         ('wp_it_id', '!=', False), 
-        ('product_id.default_code', '=', 'SB00BG'), # TODO remove
+        #('product_id.default_code', '=', 'SB00BG'), # TODO remove
         ])
     if not web_product_ids:
         continue
