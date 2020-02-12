3da5c068ddc93124472923e87a6632d9c5e337ca
nicothebrush <nicola.riolini@gmail.com>
2020-02-12 18:37:48 +0100

    generic bugfix (fast message)

--- script/update/stock_status/wordpress_get_data.py
+++ script/update/stock_status/wordpress_get_data.py
@@ -20,13 +20,13 @@ wordpress_url = config.get('wordpress', 'url')
 consumer_key = config.get('wordpress', 'key')
 consumer_secret = config.get('wordpress', 'secret')
 
-for root, folders, files in os.path('./config'):
+for root, folders, files in os.path.walk('./config'):
     for filename in files:
         if filename == 'wordpress.cfg':
             continue
         company = database.split('.')[0]       
         database[company] = os.path.join(root, filename)
-    break    
+    break
 
 # -----------------------------------------------------------------------------
 # Spaziogiardino
