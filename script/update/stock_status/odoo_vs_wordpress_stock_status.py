63f8a2b40993fc14fac86a839d2a87235a5949d3
nicothebrush <nicola.riolini@gmail.com>
2020-07-02 12:05:40 +0200

    generic bugfix (fast message)

--- script/update/stock_status/odoo_vs_wordpress_stock_status.py
+++ script/update/stock_status/odoo_vs_wordpress_stock_status.py
@@ -177,7 +177,7 @@ for company in database:
                         company, default_code, product.name,
                         status, stock_quantity, stock_comment,
                     ))
-                continue  # TODO remove
+                # continue  # TODO remove
 
                 multiplier = variation.price_multi or 1
                 if multiplier > 1:
