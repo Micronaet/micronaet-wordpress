f83993c8a7d412b66a28cbf56d5c029763c3d1c7
nicothebrush <nicola.riolini@gmail.com>
2020-09-11 11:13:36 +0200

    generic bugfix (fast message)

--- wp_connector/wp_base.py
+++ wp_connector/wp_base.py
@@ -1041,7 +1041,6 @@ class ProductProductWebServer(orm.Model):
     def update_wp_volume(self, cr, uid, ids, context=None):
         """ Update volume field
         """
-        import pdb; pdb.set_trace()
         for item in self.browse(cr, uid, ids, context=context):
             product = item.product_id
             multi = item.price_multi or 1
