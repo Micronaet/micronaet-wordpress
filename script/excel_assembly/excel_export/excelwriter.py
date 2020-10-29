3b19ece8ca4bca9bd462d1e86a238b42508f9c3a
nicothebrush <nicola.riolini@gmail.com>
2020-10-29 12:37:09 +0100

    pep 8 normalitazion

--- script/excel_assembly/excel_export/excelwriter.py
+++ script/excel_assembly/excel_export/excelwriter.py
@@ -26,7 +26,7 @@ from xlsxwriter.utility import xl_rowcol_to_cell
 from datetime import datetime, timedelta
 
 
-class ExcelWriter():
+class ExcelWriter:
     """ Class for manage creation of Excel document
     """
     # -------------------------------------------------------------------------
@@ -47,11 +47,11 @@ class ExcelWriter():
         """ Log operation for the class
         """
         if self._verbose:
-            print '%s. [%s] %s' % (
+            print('%s. [%s] %s' % (
                 datetime.now(),
                 mode.upper(),
                 message,
-                )
+                ))
 
     def _create_workbook(self):
         """ Create workbook in a temp file
@@ -86,7 +86,7 @@ class ExcelWriter():
             value[:4],
             )
 
-    def format_hour(self, value, hhmm_format=True, approx = 0.001,
+    def format_hour(self, value, hhmm_format=True, approx=0.001,
             zero_value='0:00'):
         """ Format hour HH:MM
         """
@@ -100,7 +100,6 @@ class ExcelWriter():
         hour = int(value)
         minute = int((value - hour) * 60)
         return '%d:%02d' % (hour, minute)
-
 
     def close_workbook(self, ):
         """ Close workbook
@@ -128,7 +127,7 @@ class ExcelWriter():
         self._WS[name] = self._WB.add_worksheet(name)
         self._log_operation('New WS: %s' % name)
 
-    def merge_cell(self, WS_name, rectangle, default_format=False, data=''):
+    def merge_cell(self, ws_name, rectangle, default_format=False, data=''):
         """ Merge cell procedure:
             WS: Worksheet where work
             rectangle: list for 2 corners xy data: [0, 0, 10, 5]
@@ -137,10 +136,10 @@ class ExcelWriter():
         rectangle.append(data)
         if default_format:
             rectangle.append(default_format)
-        self._WS[WS_name].merge_range(*rectangle)
+        self._WS[ws_name].merge_range(*rectangle)
         return
 
-    def write_xls_line(self, WS_name, row, line, default_format=False, col=0):
+    def write_xls_line(self, ws_name, row, line, default_format=False, col=0):
         """ Write line in excel file:
             WS: Worksheet where find
             row: position where write
@@ -154,51 +153,51 @@ class ExcelWriter():
                 record = ''
             if type(record) not in (list, tuple):
                 if default_format:
-                    self._WS[WS_name].write(row, col, record, default_format)
+                    self._WS[ws_name].write(row, col, record, default_format)
                 else:
-                    self._WS[WS_name].write(row, col, record)
+                    self._WS[ws_name].write(row, col, record)
             elif len(record) == 2: # Normal text, format
-                self._WS[WS_name].write(row, col, *record)
+                self._WS[ws_name].write(row, col, *record)
             else: # Rich format TODO
-                self._WS[WS_name].write_rich_string(row, col, *record)
+                self._WS[ws_name].write_rich_string(row, col, *record)
             col += 1
         return True
 
-    def write_xls_data(self, WS_name, row, col, data, default_format=False):
+    def write_xls_data(self, ws_name, row, col, data, default_format=False):
         """ Write data in row col position with default_format
 
             @return: nothing
         """
         if default_format:
-            self._WS[WS_name].write(row, col, data, default_format)
+            self._WS[ws_name].write(row, col, data, default_format)
         else:
-            self._WS[WS_name].write(row, col, data, default_format)
+            self._WS[ws_name].write(row, col, data, default_format)
         return True
 
-    def column_width(self, WS_name, columns_w, col=0):
+    def column_width(self, ws_name, columns_w, col=0):
         """ WS: Worksheet passed
             columns_w: list of dimension for the columns
         """
         for w in columns_w:
-            self._WS[WS_name].set_column(col, col, w)
+            self._WS[ws_name].set_column(col, col, w)
             col += 1
         return True
 
-    def row_height(self, WS_name, row_list, height=10):
+    def row_height(self, ws_name, row_list, height=10):
         """ WS: Worksheet passed
             columns_w: list of dimension for the columns
         """
         if type(row_list) in (list, tuple):
             for row in row_list:
-                self._WS[WS_name].set_row(row, height)
+                self._WS[ws_name].set_row(row, height)
         else:
-            self._WS[WS_name].set_row(row_list, height)
+            self._WS[ws_name].set_row(row_list, height)
         return True
 
-    def write_formula(self, WS_name, row, col, formula, default_format, value):
+    def write_formula(self, ws_name, row, col, formula, default_format, value):
         """ Write formula in cell passed
         """
-        return self._WS[WS_name].write_formula(
+        return self._WS[ws_name].write_formula(
             row, col, formula, default_format, value)
 
     def set_format(
@@ -234,12 +233,12 @@ class ExcelWriter():
         self._log_operation('Get format WB type')
         WB = self._WB
 
-        F = self._default_format # readability
+        F = self._default_format  # readability
 
         # Save database in self:
         create = False
         try:
-            if not self._wb_format: # raise error if not present
+            if not self._wb_format:  # raise error if not present
                 create = True
         except:
             create = True
@@ -249,7 +248,7 @@ class ExcelWriter():
                 # -------------------------------------------------------------
                 # Used when key not present:
                 # -------------------------------------------------------------
-                'default' : WB.add_format({ # Usually text format
+                'default': WB.add_format({  # Usually text format
                     'font_name': F['text'][0],
                     'font_size': F['text'][1],
                     'font_color': F['text'][2],
@@ -259,7 +258,7 @@ class ExcelWriter():
                 # -------------------------------------------------------------
                 #                       TITLE:
                 # -------------------------------------------------------------
-                'title' : WB.add_format({
+                'title': WB.add_format({
                     'bold': True,
                     'font_name': F['title'][0],
                     'font_size': F['title'][1],
@@ -277,9 +276,9 @@ class ExcelWriter():
                     'font_color': F['header'][2],
                     'align': 'center',
                     'valign': 'vcenter',
-                    'bg_color': '#849bff', #'#cfcfcf', # grey
+                    'bg_color': '#849bff',  # '#cfcfcf', # grey
                     'border': F['border'],
-                    #'text_wrap': True,
+                    # 'text_wrap': True,
                     }),
 
                 # -------------------------------------------------------------
@@ -347,7 +346,7 @@ class ExcelWriter():
                     'font_color': F['text'][2],
                     'border': F['border'],
                     'align': 'left',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'text_center': WB.add_format({
                     'font_name': F['text'][0],
@@ -355,7 +354,7 @@ class ExcelWriter():
                     'font_color': F['text'][2],
                     'border': F['border'],
                     'align': 'center',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'text_right': WB.add_format({
                     'font_name': F['text'][0],
@@ -363,7 +362,7 @@ class ExcelWriter():
                     'font_color': F['text'][2],
                     'border': F['border'],
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
 
                 'text_total': WB.add_format({
@@ -389,7 +388,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'bg_color': '#FFFFFF',
                     'align': 'left',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_blue': WB.add_format({
                     'bold': True,
@@ -399,7 +398,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'bg_color': '#c4daff',
                     'align': 'left',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_red': WB.add_format({
                     'bold': True,
@@ -419,7 +418,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'bg_color': '#b1f9c1',
                     'align': 'left',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_yellow': WB.add_format({
                     'bold': True,
@@ -429,7 +428,7 @@ class ExcelWriter():
                     'font_color': 'black',
                     'bg_color': '#fffec1',
                     'align': 'left',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_orange': WB.add_format({
                     'bold': True,
@@ -439,7 +438,7 @@ class ExcelWriter():
                     'font_color': 'black',
                     'bg_color': '#fcdebd',
                     'align': 'left',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_red_number': WB.add_format({
                     'bold': True,
@@ -449,7 +448,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'bg_color': '#ffc6af',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_green_number': WB.add_format({
                     'bold': True,
@@ -459,7 +458,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'bg_color': '#b1f9c1',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_yellow_number': WB.add_format({
                     'bold': True,
@@ -467,9 +466,9 @@ class ExcelWriter():
                     'font_size': F['text'][1],
                     'border': F['border'],
                     'font_color': 'black',
-                    'bg_color': '#fffec1',##ffff99',
+                    'bg_color': '#fffec1',  # #ffff99',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_orange_number': WB.add_format({
                     'bold': True,
@@ -479,7 +478,7 @@ class ExcelWriter():
                     'font_color': 'black',
                     'bg_color': '#fcdebd',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_white_number': WB.add_format({
                     'bold': True,
@@ -489,7 +488,7 @@ class ExcelWriter():
                     'font_color': 'black',
                     'bg_color': '#FFFFFF',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'bg_blue_number': WB.add_format({
                     'bold': True,
@@ -497,9 +496,9 @@ class ExcelWriter():
                     'font_size': F['text'][1],
                     'border': F['border'],
                     'font_color': 'black',
-                    'bg_color': '#c4daff',##ffff99',
+                    'bg_color': '#c4daff',  # #ffff99',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
 
                 # TODO remove?
@@ -512,7 +511,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'num_format': F['number'],
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
 
                 # --------------
@@ -525,7 +524,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'align': 'left',
                     'valign': 'vcenter',
-                    #'text_wrap': True
+                    # 'text_wrap': True
                     }),
                 'text_blue': WB.add_format({
                     'font_name': F['text'][0],
@@ -534,7 +533,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'align': 'left',
                     'valign': 'vcenter',
-                    #'text_wrap': True
+                    # 'text_wrap': True
                     }),
                 'text_red': WB.add_format({
                     'font_name': F['text'][0],
@@ -543,7 +542,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'align': 'left',
                     'valign': 'vcenter',
-                    #'text_wrap': True
+                    # 'text_wrap': True
                     }),
                 'text_green': WB.add_format({
                     'font_color': '#328238', ##99cc66
@@ -552,7 +551,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'align': 'left',
                     'valign': 'vcenter',
-                    #'text_wrap': True
+                    # 'text_wrap': True
                     }),
                 'text_grey': WB.add_format({
                     'font_color': '#eeeeee',
@@ -561,7 +560,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'align': 'left',
                     'valign': 'vcenter',
-                    #'text_wrap': True
+                    # 'text_wrap': True
                     }),
                 'text_wrap': WB.add_format({
                     'font_color': 'black',
@@ -570,7 +569,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'align': 'left',
                     'valign': 'vcenter',
-                    #'text_wrap': True,
+                    # 'text_wrap': True,
                     }),
 
                 # -------------------------------------------------------------
@@ -582,7 +581,7 @@ class ExcelWriter():
                     'border': F['border'],
                     'num_format': F['number'],
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
 
                 # ----------------
@@ -595,7 +594,7 @@ class ExcelWriter():
                     'num_format': F['number'],
                     'font_color': 'black',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'number_blue': WB.add_format({
                     'font_name': F['text'][0],
@@ -604,7 +603,7 @@ class ExcelWriter():
                     'num_format': F['number'],
                     'font_color': 'blue',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'number_grey': WB.add_format({
                     'font_name': F['text'][0],
@@ -613,7 +612,7 @@ class ExcelWriter():
                     'num_format': F['number'],
                     'font_color': 'grey',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'number_red': WB.add_format({
                     'font_name': F['text'][0],
@@ -622,7 +621,7 @@ class ExcelWriter():
                     'num_format': F['number'],
                     'font_color': 'red',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 'number_green': WB.add_format({
                     'font_name': F['text'][0],
@@ -631,7 +630,7 @@ class ExcelWriter():
                     'num_format': F['number'],
                     'font_color': 'green',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
 
                 'number_total': WB.add_format({
@@ -643,7 +642,7 @@ class ExcelWriter():
                     'num_format': F['number'],
                     'bg_color': '#DDDDDD',
                     'align': 'right',
-                    #'valign': 'vcenter',
+                    # 'valign': 'vcenter',
                     }),
                 }
 
@@ -655,4 +654,3 @@ class ExcelWriter():
                 )
         else:
             return True
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
