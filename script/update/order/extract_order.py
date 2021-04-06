08cab824647d992eeda14973627d7a5b1b81aea1
nicothebrush <nicola.riolini@gmail.com>
2021-04-06 15:10:16 +0200

    add some fields

--- script/update/order/extract_order.py
+++ script/update/order/extract_order.py
@@ -40,8 +40,6 @@ for root, folders, files in os.walk('..'):
         company = cfg_file.split('.')[1]
         f_log = open(os.path.join('./log', 'export.%s' % company), 'a')
 
-        log_message(f_log, 'Updating order for %s company' % company)
-
         cfg_file = os.path.expanduser(os.path.join('../', cfg_file))
         config = ConfigParser.ConfigParser()
         config.read([cfg_file])
@@ -65,22 +63,55 @@ for root, folders, files in os.walk('..'):
             'product': odoo.model('product.product'),
             'web': odoo.model('product.product.web.server'),
         }
-        log_message(f_log, 'Connect with ODOO, company %s\n' % company)
+        log_message(f_log, 'Connected with ODOO, company %s\n' % company)
     break
 
 # Use first company order only:
 order_ids = odoo_db['fia']['order'].search()
 orders = odoo_db['fia']['order'].browse(order_ids)
 order_file = open(os.path.join('./data', 'wordpress.order.csv'), 'a')
-mask = '%-10s%-15s%8s\n'
+log_message(f_log, 'Reading %s order from company %s\n' % (
+    len(order_ids), company))
+
+mask = '%-10s%-15s%8s%1s%-30s%-30s%-16s%-30s%-5s%-30s%-8s%-35s%-30s' \
+       '%-18s%-35s%-20s%-30s%-5s' \
+       '%-10.2f%-10.2f%-10.2f' \
+       '%-10s%-10.2f%-1s%-10s%-10s\n'
+
 for order in orders:
     for line in order.line_ids:
         product = get_product(line, odoo_db)
         web_product = get_web_product(line, odoo_db)
         order_file.write(mask % (
-            order.name or '',
-            order.state or '',
-            (order.date_order or '').replace('-', ''),
+            # Header:
+            (order.name or '')[:10],  # Order number
+            (order.state or '')[:15],  # State of order (Causale)
+            (order.date_order or '').replace('-', ''),  # Date order (AAAAMMGG)
+            'F',  # TODO A or F (Azienda or Persona Fisica)
+            (order.partner_name or '')[:30],  # Last name
+            ('')[:30],  # First name (non importato, unito al cognome)
+            ('')[:16],  # Fiscal code (non presente)
+            (order.billing or '')[:30],  # Address
+            ('')[:5],  # ZIP
+            ('')[:30],  # City
+            '',  # Birthday (AAAAMMGG) Non presente
+            (order.partner_email or '')[:35],  # Email
+            (order.partner_phone or '')[:30],  # Phone
+
+            # Line:
+            (line.sku or '')[:18],  # SKU
+            (line.name or '')[:35],  # Product description
+            ('')[:20],  # Brand
+            ('')[:30],  # Category (more than one!)
+            ('NR')[:5],  # UOM  (sempre NR?)
+            line.quantity,  # Q.  (10.2)
+            '',  # Cost  (10.2)
+            line.list_price,  # List price  (10.2)
+
+            # Footer
+            (order.payment)[:10],  # Payment
+            (order.real_shipping_total or order.shipping_total),  # Trans. 10.2
+            '',  # S = esente IVA
+            ('')[:10],  # Sconto ordine
+            ('')[:10],  # Coupon sconto
         ))
-
-
