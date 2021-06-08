80c1fbd5a91d349dec1e9ddae4080419fb6a2dcd
nicothebrush <nicola.riolini@gmail.com>
2021-06-08 18:40:57 +0200

    generic bugfix (fast message)

--- wp_order/import_order_wordpress.py
+++ wp_order/import_order_wordpress.py
@@ -1250,8 +1250,6 @@ class ConnectorServer(orm.Model):
                             }, context=force_context)
 
                     # Calc for line shipping (for all included):
-                    if number == '79422':
-                        pdb.set_trace()
                     line_quantity = float(line['quantity'])
                     line_price_lord = float(line['price'])
 
