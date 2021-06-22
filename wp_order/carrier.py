700fe0ebe52ed2be27ab726bf0da81f9a5ab2e13
nicothebrush <nicola.riolini@gmail.com>
2021-06-22 10:41:28 +0200

    generic bugfix (fast message)

--- wp_order/carrier.py
+++ wp_order/carrier.py
@@ -444,6 +444,7 @@ class WordpressSaleOrderRelationCarrier(orm.Model):
         # if not order.manage_delivery:
         #    return order.log_error(
         #        _('Order not delivery managed from ODOO'))
+        pdb.set_trace()
         if not order.parcel_detail:
             self.generate_parcel_from_order(cr, uid, ids, context=context)
 
