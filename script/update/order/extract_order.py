e21ccf362de1e1a1fcec6ced60e46660dec3569c
nicothebrush <nicola.riolini@gmail.com>
2022-04-26 18:34:02 +0200

    generic bugfix (fast message)

--- script/update/order/extract_order.py
+++ script/update/order/extract_order.py
@@ -209,7 +209,6 @@ else:
             # New shipment splitted for new total line:
             rate = current_net_total / new_total
 
-            pdb.set_trace()
             for record in order_records:
                 record.append(record[-1] * rate)
                 shipping_file.write(mask % list(record))
