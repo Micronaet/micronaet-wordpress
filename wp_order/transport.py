829f471513edbf3542ba5c85d67ce0c82b8045c4
nicothebrush <nicola.riolini@gmail.com>
2022-04-29 13:05:11 +0200

    generic bugfix (fast message)

--- wp_order/transport.py
+++ wp_order/transport.py
@@ -158,11 +158,29 @@ class SaleOrderCarrierZoneExtra(orm.Model):
             ('fuel', 'Carburante (perc.)'),
             ('pallet', 'Pallet non sovrapponibile (valore)'),
 
+            ('zone', 'Zona'),
             ('weight', 'Peso'),
             ('1dimension', 'Dimensione max <='),
             ('2dimension', 'Min + max dime. <='),
             ('3dimension', 'Somma dimensioni <='),
         ], 'Modalità', required=True),
+
+        'value': fields.float(
+            'Valore filtro', digits=(10, 2),
+            attrs="{'readonly': ["
+                  "(mode', 'not in', "
+                  "('weight', '1dimension', '2dimension', '3dimension'))], "
+                  "'required': [(mode', 'in', "
+                  "('weight', '1dimension', '2dimension', '3dimension'))]}",
+            help='Dove serve indicare il valore da usare per il filtro '
+                 'modalità'),
+        'value_zone_id': fields.many2one(
+            'sale.order.carrier.zone', 'Zona',
+            attrs="{'readonly': [(mode', '!=', 'zone')], "
+                  "'required': [(mode', '=', 'zone')]}",
+            help='Nel caso di filtro in modalità zona, indicare '
+                 'quella da usare'),
+
         'price': fields.float(
             'Sovrapprezzo', digits=(10, 2), required=True,
             help='Maggiorazione applicata alla zona standard calcolata '
