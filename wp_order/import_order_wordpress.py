11f900015749d7893b12f0c6455e207045d4d29b
nicothebrush <nicola.riolini@gmail.com>
2022-05-20 10:32:51 +0200

    generic bugfix (fast message)

--- wp_order/import_order_wordpress.py
+++ wp_order/import_order_wordpress.py
@@ -44,6 +44,40 @@ class WordpressSaleOrder(orm.Model):
     _description = 'Wordpress order'
     _order = 'name desc'
 
+    # Utility:
+    def send_pdf_to_printer(self, cr, uid, ids, order, fullname, context=None):
+        """ Procedure used from all print_label function for sent to printer
+            the PDF filename
+        """
+        # ---------------------------------------------------------------------
+        # Print file:
+        # ---------------------------------------------------------------------
+        filename = os.path.basename(fullname)
+        user = self.pool.get('res.users').browse(
+            cr, uid, uid, context=context)
+
+        printer_code = \
+            order.courier_mode_id.cups_printer_id.code or \
+            order.courier_supplier_id.cups_printer_id.code or \
+            order.carrier_mode_id.cups_printer_id.code or \
+            order.carrier_supplier_id.cups_printer_id.code or \
+            order.carrier_connection_id.cups_printer_id.code
+
+        # Check if need to print or to save:
+        company = user.company_id
+        if company.carrier_save_label:  # todo needed?
+            saved_path = os.path.join(
+                os.path.expanduser(company.carrier_save_label_path),
+                printer_code,
+            )
+            os.system('mkdir -p %s' % saved_path)  # Create path
+            saved_fullname = os.path.join(saved_path, filename)
+            shutil.copy(fullname, saved_fullname)
+            _logger.warning('Saved label in: %s' % saved_fullname)
+        else:
+            return self.send_report_to_cups_printer(
+                cr, uid, ids, fullname, printer_code, context=context)
+
     def print_label(self, cr, uid, ids, context=None):
         """ Extract label
         """
@@ -1365,38 +1399,6 @@ class ConnectorServer(orm.Model):
             return excel_pool.return_attachment(cr, uid, 'wordpress_order')
 
     # Override function to get sold status
-    def send_pdf_to_printer(self, cr, uid, ids, order, fullname, context=None):
-        """ Procedure used from all print_label function for sent to printer
-            the PDF filename
-        """
-        # ---------------------------------------------------------------------
-        # Print file:
-        # ---------------------------------------------------------------------
-        filename = os.path.basename(fullname)
-        user = self.pool.get('res.users').browse(
-            cr, uid, uid, context=context)
-
-        printer_code = \
-            order.courier_mode_id.cups_printer_id.code or \
-            order.courier_supplier_id.cups_printer_id.code or \
-            order.carrier_mode_id.cups_printer_id.code or \
-            order.carrier_supplier_id.cups_printer_id.code or \
-            order.carrier_connection_id.cups_printer_id.code
-
-        # Check if need to print or to save:
-        company = user.company_id
-        if company.carrier_save_label:  # todo needed?
-            saved_path = os.path.join(
-                os.path.expanduser(company.carrier_save_label_path),
-                printer_code,
-            )
-            os.system('mkdir -p %s' % saved_path)  # Create path
-            saved_fullname = os.path.join(saved_path, filename)
-            shutil.copy(fullname, saved_fullname)
-            _logger.warning('Saved label in: %s' % saved_fullname)
-        else:
-            return self.send_report_to_cups_printer(
-                cr, uid, ids, fullname, printer_code, context=context)
 
     def sold_product_on_website(self, cr, uid, ids, context=None):
         """ Return sold product for default_code
