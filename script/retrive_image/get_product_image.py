24f32cc7d2e57942286fa309941a74749f8f7091
nicothebrush <nicola.riolini@gmail.com>
2024-02-19 15:42:51 +0100

    generic bugfix (fast message)

--- script/retrive_image/get_product_image.py
+++ script/retrive_image/get_product_image.py
@@ -46,7 +46,7 @@ wcapi = woocommerce.API(
     timeout=600,
     verify_ssl=False,
     )
-parameter = {'per_page': 10, 'page': 1}
+parameter = {'per_page': 1, 'page': 1}
 
 # log_f = open(os.path.join(image_path, 'log.csv'), 'w')
 pickle_filename = './history.pickle'
